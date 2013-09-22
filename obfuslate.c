1ae8162c5f75adf81f8229f19c31074522737984
Christopher Hoobin <christopher.hoobin@rmit.edu.au>
2013-09-22 11:34:54 +1000

    Aligned definitions and function to spell inflate.
    Rearranged variable names from a to t.

--- obfuslate.c
+++ obfuslate.c
@@ -1,7 +1,11 @@
-#define w while
-#define N j=0;w(j<a)putchar(p[j++]);a=0;
+int a,b,c,d,e,f,g,h,i,j,k,l,m,n[16],o[320],p[37268];struct{int q[288];int r[16];}s,t;
+
+#define I while
+#define N j=0;I(j<a)putchar(p[j++]);a=0;
 #define F(n)p[a++]=n;if(a==37268){N}
-#define L(n)({w(g<n){j=getchar();b|=j<<g;g+=8;}j=b&((1<<n)-1);b>>=n;g-=n;j;})
-#define A(n)({j=c=k=0;w(1){k++;j=j<<1|L(1);if(j<n.t[k])break;c+=n.t[k];j-=n.t[k];}n.s[c+j];})
-#define T(h,l,n)k=0;w(k<16)h.t[k++]=0;k=0;w(k<n)h.t[(l)[k++]]++;m[1]=0;k=2;w(k<16){m[k]=m[k-1]+h.t[k-1];k++;}k=0;w(k<n){if((l)[k]>0)h.s[m[(l)[k]]++]=k;k++;}
-int a,b,c,d,e,f,g,h[320],i,j,k,l,m[16],n,o,p[37268];struct{int s[288];int t[16];}u,v;E(int l,int d){c=0;if(a<d)c+=37268;c+=a-d;k=0;w(k<l){p[a++]=p[c++];if(a==37268){N;E(l-k-1,d);return;}if(c==37268)c=0;k++;}}main(){L(24);o=L(8);L(24);L(24);if(o&4)L(L(8)|L(8)<<8);if(o&8)w(L(8));if(o&16)w(L(8));if(o&2)L(16);w(!f){f=L(1);l=L(2);if(l==0){n=g%8;if(n>0){b>>=n;g-=n;}i=L(8)|L(8)<<8;L(16);w(i-->0){F(L(8))}continue;}if(l==1){o=0;w(o<144)h[o++]=8;w(o<256)h[o++]=9;w(o<280)h[o++]=7;w(o<288)h[o++]=8;T(u,h,288)}if(l==2){e=L(5)+257;d=L(5)+1;k=L(4)+4;bzero(h,76);i=0;w(i<k)h[i++["qrsaihjgkflemdncobp"]-97]=L(3);T(u,h,19)i=0;w(i<e+d){o=A(u);k=3;n=2;c=0;if(o<16){h[i++]=o;continue;}if(o==16)c=h[i-1];if(o==17)n++;if(o>17){k=11;n=7;}k+=L(n);j=0;w(j++<k)h[i++]=c;}T(u,h,e)T(v,h+e,d)}w(1){o=A(u);if(o<256){F(o)continue;}if(o==256)break;o-=257;if(o<9)e=o+3;else if(o==28)e=258;else{i=1;j=13;w(o>=j){j+=4;i++;}e=(1<<(i-1))*10+3+(o-j+4)*(1<<i);}n=0;if(o>7&&o<28)n=(o-4)/4;if(n>0)e+=L(n);if(l==1)o=((L(5)<<3)*8623620610ULL&1136090292240ULL)%1023;else o=A(v);n=0;if(o<4)d=o+1;else{n=(o>>1)-1;d=(1<<(n+1))+1+((o&1)<<n);}if(n>0)d+=L(n);E(e,d);}}N}
+#define L(n)({I(g<n){j=getchar();b|=j<<g;g+=8;}j=b&((1<<n)-1);b>>=n;g-=n;j;})
+#define A(n)({j=c=k=0;I(1){k++;j=j<<1|L(1);if(j<n.r[k])break;c+=n.r[k];j-=n.r[k];}n.q[c+j];})
+#define T(h,l,m)k=0;I(k<16)h.r[k++]=0;k=0;I(k<m)h.r[(l)[k++]]++;n[1]=0;k=2;I(k<16){n[k]=n[k-1]+h.r[k-1];k++;}k=0;I(k<m){if((l)[k]>0)h.q[n[(l)[k]]++]=k;k++;}
+        E(int l,int d){c=0;if(a<d)c+=37268;c+=a-d;k=0;I(k<l){p[a++]=p[c++];if(a==37268){N;E(l-k-1,d);return;}if(c==37268)c=0;k++;}}
+
+main(){L(24);h=L(8);L(24);L(24);if(h&4)L(L(8)|L(8)<<8);if(h&8)I(L(8));if(h&16)I(L(8));if(h&2)L(16);I(!f){f=L(1);l=L(2);if(l==0){m=g%8;if(m>0){b>>=m;g-=m;}i=L(8)|L(8)<<8;L(16);I(i-->0){F(L(8))}continue;}if(l==1){h=0;I(h<144)o[h++]=8;I(h<256)o[h++]=9;I(h<280)o[h++]=7;I(h<288)o[h++]=8;T(s,o,288)}if(l==2){e=L(5)+257;d=L(5)+1;k=L(4)+4;bzero(o,76);i=0;I(i<k)o[i++["qrsaihjgkflemdncobp"]-97]=L(3);T(s,o,19)i=0;I(i<e+d){h=A(s);k=3;m=2;c=0;if(h<16){o[i++]=h;continue;}if(h==16)c=o[i-1];if(h==17)m++;if(h>17){k=11;m=7;}k+=L(m);j=0;I(j++<k)o[i++]=c;}T(s,o,e)T(t,o+e,d)}I(1){h=A(s);if(h<256){F(h)continue;}if(h==256)break;h-=257;if(h<9)e=h+3;else if(h==28)e=258;else{i=1;j=13;I(h>=j){j+=4;i++;}e=(1<<(i-1))*10+3+(h-j+4)*(1<<i);}m=0;if(h>7&&h<28)m=(h-4)/4;if(m>0)e+=L(m);if(l==1)h=((L(5)<<3)*8623620610ULL&1136090292240ULL)%1023;else h=A(t);m=0;if(h<4)d=h+1;else{m=(h>>1)-1;d=(1<<(m+1))+1+((h&1)<<m);}if(m>0)d+=L(m);E(e,d);}}N}
