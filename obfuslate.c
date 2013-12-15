6ac3544fc10ac195595802c202fa8e2ce905df32
Christopher Hoobin <christopher@hoob.in>
2013-12-16 04:48:54 +1100

    Incorrect window size. s/37267/32768/g. Gailly and Adler would have my head!

--- obfuslate.c
+++ obfuslate.c
@@ -2,14 +2,14 @@
 
 #define I while
 #define N d=0;I(d<a)putchar(e[d++]);a=0;
-#define F(n)e[a++]=n;if(a==37268){N}
+#define F(n)e[a++]=n;if(a==32768){N}
 #define L(n)({int b;I(g<n){b=getchar();k|=b<<g;g+=8;}b=k&((1<<n)-1);k>>=n;g-=n;b;})
 #define A(n)({d=c=b=0;I(1){b++;d=d<<1|L(1);if(d<n.r[b])break;c+=n.r[b];d-=n.r[b];}n.q[c+d];})
 #define T(i,j,k){n[b]=n[b-1]+i.r[b-1];b++;}b=0;I(b<k){if(b[(j)]>0)i.q[n[b[(j)]]++]=b;b++;}
 #define E(i,j,k)b=0;I(b<16)b++[i.r]=0;b=0;I(b<k)i.r[b++[(j)]]++;n[1]=0;b=2;I(b<16)T(i,j,k)
 
-int a,b,c,d,e[37268];void _(int i,int j){c=0;if(a<j)c+=37268;c+=a-j;b=0;I(b<i)
-{e[a++]=e[c++];if(a==37268){N;_(i-b-1,j);return;}if(c==37268)c=0;b++;}}int f,g
+int a,b,c,d,e[32768];void _(int i,int j){c=0;if(a<j)c+=32768;c+=a-j;b=0;I(b<i)
+{e[a++]=e[c++];if(a==32768){N;_(i-b-1,j);return;}if(c==32768)c=0;b++;}}int f,g
 ,h,i,j,k,l,m,n[16],o[320],p;struct{int q[288];int r[16];}s,t;int main(){L(24);
 h=L(8);L(24);L(24);if(h&4)L((L(8)|L(8)<<8));if(h&8)I(L(8));if(h&16)I(L(8));if(
 h&2)L(16);I(!f){f=L(1);l=L(2);if(l==0){m=g%8;if(m>0){k>>=m;g-=m;}i=L(8)|L(8)<<
